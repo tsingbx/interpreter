436fbd2e76ed4ee86fc732003b9bb16da14e3529
tsingbx <tsingbx@users.noreply.github.com>
2024-01-28 10:08:14 +0800

    support function

--- object/environment.go
+++ object/environment.go
@@ -1,11 +1,15 @@
 package object
 
 type Environment struct {
+	outer *Environment
 	store map[string]Object
 }
 
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
 	return obj, ok
 }
 
@@ -16,5 +20,11 @@ func (e *Environment) Set(name string, val Object) Object {
 
 func NewEnvironment() *Environment {
 	s := make(map[string]Object)
-	return &Environment{store: s}
+	return &Environment{store: s, outer: nil}
+}
+
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outer
+	return env
 }
